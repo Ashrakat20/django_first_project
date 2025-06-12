c56b4437f8d686e731bf8273f1a663a08b65ebef
[ashrakat saeed] <[ashrakatsaeed177@gmail.com]>
2025-06-12 15:18:13 +0300

    serializing realtionships

--- store/serializer.py
+++ store/serializer.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
+from .models import Product, Collections
+from decimal import Decimal
 
+
+class CollectionsSerializer(serializers.Serializer):
+    id=serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    
 class ProductSerializer(serializers.Serializer):
     id=serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price =serializers.DecimalField(max_digits=6,decimal_places=2)
+    Price =serializers.DecimalField(max_digits=6,decimal_places=2, source='unit_price')
+    unit_price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.HyperlinkedRelatedField(
+        queryset= Collections.objects.all(),
+        view_name= 'collection_detail'
+    )
+    def calculate_tax(self, product):
+        return product.unit_price*Decimal(1.1)
