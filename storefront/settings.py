82003490fd037719985fde71d3b75f5edb4d2d11
[ashrakat saeed] <[ashrakatsaeed177@gmail.com]>
2025-06-18 13:00:35 +0300

    djoser user authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -152,3 +152,8 @@ AUTH_USER_MODEL='core.user'
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+DJOSER = {
+    'SERIALIZERS':{
+        'user_create':'core.serializers.UserCreateSerializer'
+    }
+}
